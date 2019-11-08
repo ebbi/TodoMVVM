94f9fae4c16471b10ddc2d12d4b0e5a5ba089576
Ebbi <ebbi40@gmail.com>
2019-11-08 23:54:34 +0000

    Entity Todo created

---
+++ app/src/main/java/com/example/todomvvm/Todo.java
@@ -0,0 +1,42 @@
+package com.example.todomvvm;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+/**
+ * A basic class representing a one-column todo_database table.
+ *
+ * @ Entity - You must annotate the class as an entity and supply a table name if not class name.
+ * @ PrimaryKey - You must identify the primary key.
+ * @ ColumnInfo - You must supply the column name if it is different from the variable name.
+ *
+ * See the documentation for the full set of annotations.
+ * https://developer.android.com/topic/libraries/architecture/room.html
+ */
+
+@Entity(tableName = "todo_table")
+public class Todo {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "title")
+    private String mTitle;
+    private String mDetail;
+    
+    public Todo(@NonNull String title) {
+        this.mTitle = title;
+    }
+    public String getTitle() {
+        return this.mTitle;
+    }
+
+    public String getDetail() {
+        return this.mDetail;
+    }
+    public void setDetail(String mDetail) {
+        this.mDetail = mDetail;
+    }
+
+}
